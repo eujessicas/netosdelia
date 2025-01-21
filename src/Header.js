e0cbbcbf766120919a3adac178d5e2dea8591da9
Jessica <jjessicammayara@gmail.com>
2025-01-20 21:22:08 -0300

    primeira alteração

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './header.css';
+
+const Header = () => {
+    return(
+        <header className="header">
+            <div className="logo"> 
+                <h1>Netos de Lia</h1>
+            </div>
+            
+        </header>
+    )
+}
+
+
+    
